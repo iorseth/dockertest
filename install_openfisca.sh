0f5d87b179e0ac75585344515ec3e8f03bdc9270
AlexisFA <alexis.faydel@gmail.com>
2016-05-26 16:22:51 +0200

    correction of the script name

---
+++ install_openfisca.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Current directory is /home/jovyan
+
+source /opt/conda/bin/activate python2
+
+git clone https://github.com/openfisca/openfisca-core.git
+cd openfisca-core
+pip install --editable .
+python setup.py compile_catalog
+cd ..
+
+git clone https://github.com/openfisca/openfisca-france.git
+cd openfisca-france
+pip install --editable .
+python setup.py compile_catalog
+cd ..
+
+git clone https://github.com/openfisca/openfisca-france-indirect-taxation.git
+cd openfisca-france-indirect-taxation
+pip install --editable .
+cd ..
+
